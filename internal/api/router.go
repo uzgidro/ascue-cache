c98587ac6c5fb140d4309b7d74ef723067b7133d
suxa56 <suxa5600@gmail.com>
2025-04-18 16:23:08 +0500

    add cors

--- internal/api/router.go
+++ internal/api/router.go
@@ -3,6 +3,7 @@ package api
 import (
 	"ascue/internal/redisstore"
 	"fmt"
+	"github.com/go-chi/cors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,6 +11,15 @@ import (
 
 func NewRouter(store redisstore.Store) http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	r.Get("/api/*", func(w http.ResponseWriter, r *http.Request) {
 		key := chi.URLParam(r, "*")
